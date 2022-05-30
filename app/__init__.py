74a6dad597c0ffd39442729dfe0139744b2efeec
root <root@877595-cb00678.tmweb.ru>
2022-05-30 09:16:09 +0300

    30/05/22

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,4 @@ app = Flask(__name__, static_folder="/root/coffeServer/app/web",
             template_folder='/root/coffeServer/app/web')
 app.config.from_object('config')
 db = SQLAlchemy(app)
-from app import app, ClientController, model, security, CoffeController, CoffeHouseController, flutter
+from app import app, ClientController, model, security, CoffeController, CoffeHouseController, flutter, RemoteFileManager
--- app/model.py
+++ app/model.py
@@ -1,11 +1,21 @@
 from concurrent.futures.process import _python_exit
 import email
+import json
 
 from sqlalchemy import values
 from app import db
 from app import app
 #import enum
 #from app import security as scr
+
+
+class Auth(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    phone = db.Column(db.String(120))
+    email = db.Column(db.String(120))
+    role = db.Column(db.String(120))
+    realid = db.Column(db.Integer)
+    passwordHash = db.Column(db.String(120))
 
 
 class Client(db.Model):
@@ -54,10 +64,24 @@ class Coffe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     category = db.Column(db.String)
+    description = db.Column(db.String)
     volumes = db.Column(db.String)
     suppliments = db.Column(db.String)
     coffehouse_id = db.Column(db.Integer, db.ForeignKey('coffehouse.id'))
-    photo = db.relationship('Photo', backref='coffe', lazy='dynamic')
+    photo = db.relationship('Photo', backref='coffe',
+                            lazy='dynamic', cascade="all, delete, delete-orphan")
+
+    def toDict(self):
+        myDict = {}
+        myDict['id'] = self.id
+        myDict['name'] = self.name
+        myDict['category'] = self.category
+        myDict['description'] = self.description
+        myDict['volumes'] = json.loads(self.volumes.replace("'", '"'))
+        myDict['suppliments'] = json.loads(self.suppliments.replace("'", '"'))
+        myDict['photo'] = (list(
+            map(lambda x: app.config['MEDIA_SERVER_ADDRESS']+'/'+x.filename, self.photo)))
+        return myDict
 
 
 class Photo(db.Model):
