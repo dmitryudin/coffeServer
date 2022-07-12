78f24ef5c200e1e0a3456bf353772524063becd4
root <root@877595-cb00678.tmweb.ru>
2022-07-12 10:18:04 +0300

    stable commit 12.07.22

--- app/Models/Order_model.py
+++ app/Models/Order_model.py
@@ -1,9 +1,14 @@
 
+from pkg_resources import require
 from app import db
 
 
 class Order(db.Model):
     __tablename__ = 'order'
     id = db.Column(db.Integer, primary_key=True)
+    #id_payment = db.Column(db.Integer)
+    #required_datetime = db.Column(db.String)
+    #positions = db.Column(db.String)
+    #on_place = db.Column(db.Boolean)
     coffehouse_id = db.Column(db.Integer, db.ForeignKey('coffehouse.id'))
     client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
